eb3cdb585cdef708c87985f574ce68b6fab3ca6a
jeanpier1456 <martin.thummel@orange.fr>
2017-02-27 18:12:32 +0100

    G

---
+++ noteR.R
@@ -0,0 +1,7 @@
+cor(data$"X001656082"[1:35],data$"X001694169"[1:35])
+ind(data$"X001656082")
+data$"X001694169"[1:36]
+length(data$"X001694169")
+data[1,][which(!is.na(data[1,))]
+
+Test=data[,grep("Industrie",dico$def)+2]
