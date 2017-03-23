a0906e216f5285d6ed8b40d7cf09ed1a7bb9dadd
zhao tong <zt@zhaodeMacBook-Pro.local>
2017-03-23 16:34:26 +0100

    datas will be saved at the first time and then it will be loaded from files in /Data

--- python/linear_rgr.py
+++ python/linear_rgr.py
@@ -3,6 +3,9 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_regression, mutual_info_regression
 import read_file as rf
 import numpy as np
+import argparse
+import os, sys
+import pickle
 
 data_path = "../Data_M[2005-2017].csv"
 label_path = "../Dico_M[2005-2017].csv"
@@ -21,9 +24,41 @@ def mse(y, yhat):
   assert (y.shape == yhat.shape), "y and yhat should have same size"
   return np.sum(np.power((yhat - y), 2)) / y.shape[-1]
 
+
 print "\nload data: " + data_path.split('/')[-1].split('.')[0]
-labels, y_name = rf.read_label(label_path)
-dates, datas, indexs = rf.read_data(data_path, y_name, 0.3)
+
+if os.path.exists("../Data/datas.npy"):
+  # load datas
+  datas = np.load('../Data/datas.npy')
+  dates = np.load('../Data/dates.npy')
+  outfile = open('../Data/indexs.pkl', 'rb')
+  indexs = pickle.load(outfile)
+  outfile.close()
+  outfile = open('../Data/labels.pkl', 'rb')
+  labels = pickle.load(outfile)
+  outfile.close()
+  outfile = open('../Data/y_name.pkl', 'rb')
+  y_name = pickle.load(outfile)
+  outfile.close()
+
+else:
+  # load datas
+  labels, y_name = rf.read_label(label_path)
+  dates, datas, indexs = rf.read_data(data_path, y_name, 0.3)
+
+  # save datas
+  np.save('../Data/datas.npy', datas)
+  np.save('../Data/dates.npy', dates)
+  outfile = open('../Data/indexs.pkl', 'wb')
+  pickle.dump(indexs, outfile)
+  outfile.close()
+  outfile = open('../Data/labels.pkl', 'wb')
+  pickle.dump(labels, outfile)
+  outfile.close()
+  outfile = open('../Data/y_name.pkl', 'wb')
+  pickle.dump(y_name, outfile)
+  outfile.close()
+
 print "load %d data" % datas.shape[0]
 
 # seperate feature(x_train) and prediction(y_train)
