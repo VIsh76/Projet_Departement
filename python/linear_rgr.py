50716f98886bdd698a61459ac731231c5a8c3ba0
zhao tong <zt@zhaodeMacBook-Pro.local>
2017-04-10 17:29:48 +0200

    add three regression models

--- python/linear_rgr.py
+++ python/linear_rgr.py
@@ -1,6 +1,8 @@
-from sklearn.linear_model import Ridge, Lasso
+from sklearn.linear_model import Ridge, Lasso, ElasticNetCV
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_regression, mutual_info_regression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
 import read_file as rf
 import numpy as np
 import argparse
@@ -10,21 +12,31 @@ import pickle
 data_path = "../Data_M[2005-2017].csv"
 label_path = "../Dico_M[2005-2017].csv"
 
-def seperate_dataset(data_x, data_y, train_ratio):
 
+def seperate_dataset(data_x, data_y, train_ratio):
   assert (train_ratio < 1 and train_ratio > 0), "The train ratio should be between 0 and 1."
   data_num = data_x.shape[0]
   sep_ind = int(data_num * train_ratio)
   return data_x[:sep_ind - 2, :], data_y[2:sep_ind, :], data_x[sep_ind - 2:-5, :], data_y[sep_ind:-3, :]
 
-def mse(y, yhat):
 
+def mae(y, yhat):
   y = y.reshape((1, -1))
   yhat = yhat.reshape((1, -1))
   assert (y.shape == yhat.shape), "y and yhat should have same size"
-  return np.sqrt(np.sum(np.power(yhat - y, 2))) / y.shape[1]
+  return np.mean(np.abs(yhat - y))
+
+
+
+def mse(y, yhat):
+  y = y.reshape((1, -1))
+  yhat = yhat.reshape((1, -1))
+  assert (y.shape == yhat.shape), "y and yhat should have same size"
+  return np.mean(np.power((yhat - y), 2))
+
 
 print("\nload data: " + data_path.split('/')[-1].split('.')[0])
+
 
 if os.path.exists("../Data/datas.npy"):
   # load datas
@@ -39,7 +51,6 @@ if os.path.exists("../Data/datas.npy"):
   outfile = open('../Data/y_name.pkl', 'rb')
   y_name = pickle.load(outfile)
   outfile.close()
-
 else:
   # load datas
   labels, y_name = rf.read_label(label_path)
@@ -76,17 +87,59 @@ print("val  : %d cases" % x_val.shape[0])
 print("\n----------------------------------------------")
 
 print("choose data...\n")
-k = 100
+k = 10
 feature_eng = SelectKBest(mutual_info_regression, k)
 x_train_new = feature_eng.fit_transform(x_train, y_train[:,1])
 x_val_new = feature_eng.transform(x_val)
 print("keep %d feature" % k)
 print("\n----------------------------------------------")
 
-print("train model...\n")
-clf = Lasso(alpha=0.1)
+print("train LASSO model...\n")
+clf = Lasso(alpha=0.5)
 clf.fit(x_train_new, y_train[:, 1])
 yhat_train = clf.predict(x_train_new)
 yhat_val = clf.predict(x_val_new)
-print("score on train: %f" % mse(yhat_train, y_train[:, 1]))
-print("score on val: %f" % mse(yhat_val, y_val[:, 1]) )
+print("score on train: %f" % mae(yhat_train, y_train[:, 1]))
+print("score on val: %f" % mae(yhat_val, y_val[:, 1]))
+
+
+print(y_val[:, 1])
+print(yhat_val)
+
+print("\ntrain SVR model...\n")
+clf = SVR(C=1, epsilon=0.2)
+clf.fit(x_train_new, y_train[:, 1])
+yhat_train = clf.predict(x_train_new)
+yhat_val = clf.predict(x_val_new)
+print("score on train: %f" % mae(yhat_train, y_train[:, 1]))
+print("score on val: %f" % mae(yhat_val, y_val[:, 1]))
+
+
+print(y_val[:, 1])
+print(yhat_val)
+
+
+print("\ntrain decision tree model...\n")
+clf = DecisionTreeRegressor(max_depth=2)
+clf.fit(x_train_new, y_train[:, 1])
+yhat_train = clf.predict(x_train_new)
+yhat_val = clf.predict(x_val_new)
+print("score on train: %f" % mae(yhat_train, y_train[:, 1]))
+print("score on val: %f" % mae(yhat_val, y_val[:, 1]))
+
+print(y_val[:, 1])
+print(yhat_val)
+
+
+print("\ntrain elastique net model...\n")
+clf = ElasticNetCV(l1_ratio = [.5, .7, .9, .95, .99, 1], max_iter = 50000)
+clf.fit(x_train_new, y_train[:, 1])
+yhat_train = clf.predict(x_train_new)
+yhat_val = clf.predict(x_val_new)
+print("score on train: %f" % mae(yhat_train, y_train[:, 1]))
+print("score on val: %f" % mae(yhat_val, y_val[:, 1]))
+print('l1_ratio = %f' % clf.l1_ratio_)
+print('alpha = %f' % clf.alpha_) 
+
+print(y_val[:, 1])
+print(yhat_val)
